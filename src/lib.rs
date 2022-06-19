251343039281d05143be0128a41e105d10e47d46
ShinNakamura <kokoronopython@gmail.com>
2022-06-19 18:34:46 +0900

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use std::error::Error;
 use std::io::{self, Write};
 use std::collections::HashMap;
-use csv;
 
 type MyResult = Result<(), Box<dyn Error>>;
 type Record = HashMap<String, String>;
@@ -10,8 +9,8 @@ pub fn run() -> MyResult {
     let input = io::stdin();
     let input = io::BufReader::new(input.lock());
     let mut rdr = csv::Reader::from_reader(input);
-    let out = io::stdout();
-    let mut wtr = io::BufWriter::new(out.lock());
+    let stdout = io::stdout();
+    let mut wtr = io::BufWriter::new(stdout.lock());
     // ヘッダーの順番を守る
     let header = rdr.headers()?.clone();
     let mut rec_n = 0;
